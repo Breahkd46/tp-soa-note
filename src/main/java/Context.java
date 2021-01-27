04dee4a368572e7b68097eb25216ab007a6cd92a
Nicolas <fussure@gmail.com>
2021-01-27 10:59:26 +0100

    hotfix

--- src/main/java/Context.java
+++ src/main/java/Context.java
@@ -1,4 +1,6 @@
 package main.java;
+import main.java.model.Bill;
+import main.java.model.Service;
 import main.java.model.User;
 
 import java.util.ArrayList;
@@ -7,8 +9,13 @@ import java.util.List;
 public class Context{
     private static Context instance;
     private List<User> users = new ArrayList<User>();
-    private Context(){
+    private List<Service> services = new ArrayList<Service>();
+    private List<Bill> bills = new ArrayList<Bill>();
 
+     private Context(){
+        this.users = new ArrayList<User>();
+        this.services = new ArrayList<Service>();
+        this.bills = new ArrayList<Bill>();
     }
 
     public Context getContext(){
@@ -17,4 +24,30 @@ public class Context{
         }
         return instance;
     }
+
+    public List<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(List<Bill> bills) {
+        this.bills = bills;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+
+
 }
